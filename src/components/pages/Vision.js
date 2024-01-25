c7b7b7c404d52597c12d0661c719c9c70175ea47
katemesias <131848005+katemesias@users.noreply.github.com>
2024-01-25 15:12:24 +0800

    UPDATE VISION CSS AND JS

---
+++ src/components/pages/Vision.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './Vision.css';
+
+function Vision() {
+   // State variable to store the background image URL
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    // Fetch the background image URL or set it from some source
+  const imageUrl = './vision-banner2.jpg'; // Replace with the correct path to your image
+  setBackgroundImage(imageUrl);
+  }, []);
+
+  return (        
+      <div className='vision-container' style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <h1>Vision</h1>
+      <p>
+        <br />At FurryTails, we envision a world where every pet
+        <br/>enjoys a life of optimal health, happiness, and companionship, supported by compassionate and advanced veterinary care.
+          </p>
+          </div>
+  );
+};
+
+export default Vision;
