c9df28be39bb5c4aa6110b5161a71022e73ef177
itzyokdxx <131840627+itzyokdxx@users.noreply.github.com>
2024-01-25 14:01:30 +0800

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,3 +14,33 @@ function Cards() {
                 text='Wellness Exams'
                 path='/services'
               />
+               <CardsItem
+              src='/services_2.jpg'
+              text='Diagnostic and Surgery Services'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardsItem
+              src='/services_3.jpg'
+              text='Grooming Services'
+              path='/services'
+            />
+            <CardsItem
+              src='/services_4.jpg'
+              text='Health Insurance Assistance'
+              path='/products'
+            />
+            <CardsItem
+              src='/services_5.png'
+              text='Therapy and Rehabilitation'
+              path='/log-in'
+              />
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    
+    export default Cards;
