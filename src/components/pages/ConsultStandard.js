efd39e5da37ea6a6d9e6ec6dabf75598b6646061
Abe Mae <131854003+Tjkez@users.noreply.github.com>
2024-01-25 14:45:16 +0800

    Update Consult.js

---
+++ src/components/pages/ConsultStandard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ConsultStandard.css';
+
+function ConsultStandard() {
+  return (
+    <div className='standard-container'>
+      <h1>Consult Standard</h1>
+      <p>
+        For review of an on-going issue or concern that has been consulted
+        <br/>on before with one of our vets and is currently still being monitored and/or treatment is still in progress
+        <br/>For booster vaccinations
+        <br/>For follow up deworming
+        <br/>For routine testing (e.g. heartworm test, FIV/FeLV test etc)
+      </p>
+    </div>
+
+
+  );
+}
+
+export default ConsultStandard;
