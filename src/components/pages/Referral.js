c3e767e41b2ab3c3dc0956b54e0cf8cbe2e3dcae
katemesias <131848005+katemesias@users.noreply.github.com>
2024-01-25 15:11:18 +0800

    UPDATE REFERRAL CSS AND JS

---
+++ src/components/pages/Referral.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Referral.css';
+
+function Referral() {
+  return (
+    <div className='referral-container'>
+      <h1>Referral</h1>
+      <p>
+        <br/>For referral tests or procedures from non-PHH veterinarians such as blood tests, ultrasounds, or x-rays.
+        <br/>You are required to bring your vet's referral letter
+        <br/>Second opinions or full referrals (when your vet is passing a case to us and will no longer supervise and/or monitor your pet’s progress)
+        <br/>should be booked as a “consult, extended.” These will require a consultation with one of our vets so that we might get a thorough medical history from you.
+      </p>
+    </div>
+
+  );
+}
+
+export default Referral;
