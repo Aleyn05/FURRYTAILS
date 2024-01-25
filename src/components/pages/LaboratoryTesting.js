3616b5dc779f5e9a88597b41fdbbd5a891b42f96
Abe Mae <131854003+Tjkez@users.noreply.github.com>
2024-01-25 14:46:41 +0800

    Update labtesting.js

---
+++ src/components/pages/LaboratoryTesting.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LaboratoryTesting.css';
+
+function LaboratoryTesting() {
+  return (
+    <div className='laboratory-container'>
+      <h1>Laboratory</h1>
+      <p>
+        For concerns that have been seen before by one of our veterinarians
+        where laboratory tests were recommended like (but not limited to):
+        <br/>Follow up tests like CBC to monitor anemia
+        <br/>Blood chemistry to monitor kidney disease
+        <br/>Ear swab to monitor an ear infection
+        <br/>Skin scrape to monitor a parasitic skin infection
+      </p>
+    </div>
+
+
+  );
+}
+
+export default LaboratoryTesting;
